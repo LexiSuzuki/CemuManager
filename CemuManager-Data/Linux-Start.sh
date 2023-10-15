81f257b23485c23725385bdeafca3956e0683da1
LexiSuzuki <107337101+LexiSuzuki@users.noreply.github.com>
2023-10-15 07:33:50 -0400

    Add files via upload

---
+++ CemuManager-Data/Linux-Start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#---------------DUMMY CODE---------------------
+echo "Generating requierd files"
+echo "/dumy/mlc/location" > ~/.local/CemuStarter/mlc
+echo "/dummy/cemu/location" > ~/.local/CemuStarter/cemu
+sleep 2
+echo "Done"
+sleep 1
+exit
+#---------------DUMMY CODE---------------------
