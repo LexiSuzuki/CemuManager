3e5a0bcfe530d80160ae1bc34236f6d9a4608032
LexiSuzuki <107337101+LexiSuzuki@users.noreply.github.com>
2023-10-15 07:56:42 -0400

    Add files via upload

--- CemuManager-Data/MacOS-Start.sh
+++ CemuManager-Data/MacOS-Start.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-echo "Generating requierd files"
-cd ~/
-mkdir CemuManager
-cd CemuManager
-touch mlc.cfg
-touch cemu.cgf
-echo "/dumy/mlc/location" > mlc.cfg
-echo "/dummy/cemu/location" > cemu.cgf
-cd ~/Downloads/CemuManager-main
-cp CemuManager.sh ~/CemuManager
 echo "starting"
 sleep 2
 sh ./CemuManager.sh
